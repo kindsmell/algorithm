b6ab06cac916e3bb9685e006483058f9876215b9
kindsmell <134349907+kindsmell@users.noreply.github.com>
2026-04-22 21:23:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.67 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,38 +1,16 @@
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
+#include <algorithm> 
+
 using namespace std;
 
-int solution(vector<int> nums)
-{
-    unordered_map<int, int> m;
-    unordered_map<int, int> cnt_m;
+int solution(vector<int> nums) {
+    // 1. 종류를 담기
+    unordered_set<int> species(nums.begin(), nums.end());
+
+    // 2. 가질 수 있는 최대 마릿수
     int goal = nums.size() / 2;
-    int result = 0;
-    for(const auto& kind : nums) m[kind]++;
-    
-    int cnt = 0;
-    int flag = 0;
-    while(1)
-    {
-        for(auto& [kind, n] : m)
-        {
-            if(m[kind] > 0)
-            {
-                
-                m[kind]--;
-                cnt_m[kind] = 1;
-                cnt++;
-                if(cnt == goal)
-                {
-                    flag = 1;
-                    break;
-                }
-            } 
-        }
-        if(flag == 1) break;
-    }
-    
-    for(const auto& [kind, flag] : cnt_m) result += cnt_m[kind];
-        
-    return result;
+
+    // 3. 종류의 수와 goal 중 작은 값이 정답.
+    return min((int)species.size(), goal);
 }
