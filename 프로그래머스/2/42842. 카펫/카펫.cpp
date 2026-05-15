92b283cf37a5bb7b9dc0e859210c41e5d7bedf73
kindsmell <134349907+kindsmell@users.noreply.github.com>
2026-05-16 00:09:10 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    int total = brown + yellow;
+    
+    for (int height = 3; height <= sqrt(total); height++) {
+        if (total % height == 0) {
+            int width = total / height;
+            
+            if ((width - 2) * (height - 2) == yellow) {
+                answer.push_back(width);
+                answer.push_back(height);
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
