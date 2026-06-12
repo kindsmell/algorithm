5e1891cffb178a4cf7854388faaac9664fd27380
kindsmell <134349907+kindsmell@users.noreply.github.com>
2026-06-12 21:07:03 +0900

    [level 3] Title: 거스름돈, Time: 2.75 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> money) {
+    vector<int> dp(n + 1, 0);
+    
+    dp[0] = 1;
+    
+    for (int coin : money) {
+        for (int i = coin; i <= n; i++) {
+            dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+        }
+    }
+    
+    return dp[n];
+}
